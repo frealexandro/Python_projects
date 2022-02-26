d9e872eed619c35566fc4070681639b5f1420dd3
Jhoner Santiago Novoa Rojas <jhoner300@gmail.com>
2022-02-25 23:43:19 -0500

    change language

---
+++ cycle_ for.py
@@ -0,0 +1,15 @@
+# print (1)
+# print (2)
+
+
+# counter= 1
+# print (counter)
+# while counter < 100:
+#      counter += 1
+#      print (counter)
+
+# a = list(range (0 , 100))
+# print (a)
+
+for counter in range (1001 ):
+     print(counter)
